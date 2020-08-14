a590127affd1e8f5b0d2f8145d859a5c02a37b9a
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2020-08-14 20:50:45 +0200

    Add demo3
    - Add demo3 target to CMakeLists.txt.
    - Implement demo3 in C++ and ChaiScript demonstrating the use of ranges
      and axes.

---
+++ src/demo3.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+
+#include <gnuplotxx.h>
+
+using namespace gnuplotxx;
+
+int main(int argc, char *argv[]) {
+  auto plot = Plot("Plot").range(Axes::Y2, -1.5, 1.5);
+  auto series1 = plot.createSeries("x^2").plotStyle(PlotStyle::Lines);
+  auto series2 = plot.createSeries("sin(5x)")
+                     .plotStyle(PlotStyle::Lines)
+                     .smooth(Smooth::CSplines)
+                     .axes(Axes::X1Y2);
+
+  for (double x = 0; x < 5; x += 0.1) {
+    series1.append(x, x * x);
+    series2.append(x, std::sin(2 * x));
+  }
+
+  plot.update();
+
+  std::cout << "Press ENTER to exit..." << std::endl;
+  std::getchar();
+
+  return 0;
+}
