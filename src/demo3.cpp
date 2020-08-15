992edea1b8f8c245d11821276e67ed608fffb514
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2020-08-15 15:56:13 +0200

    Fix typos

--- src/demo3.cpp
+++ src/demo3.cpp
@@ -9,7 +9,7 @@ using namespace gnuplotxx;
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
   auto plot = Plot("Plot").range(Axes::Y2, -1.5, 1.5);
   auto series1 = plot.createSeries("x^2").with(PlotStyle::Lines);
-  auto series2 = plot.createSeries("sin(5x)")
+  auto series2 = plot.createSeries("sin(2x)")
                      .with(PlotStyle::Lines)
                      .smooth(Smooth::CSplines)
                      .axes(Axes::X1Y2);
